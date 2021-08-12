5d42cde07a6af84d8d099f58c0edbf3d71215df3
Juanito perez <Juanito@hotmail.com>
2021-08-12 09:26:38 -0500

    Clase 2: Sintaxis y herramienta de debug

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
-	import Datos from "./Datos.svelte";
+
+	import Datos from "./Clase_Componente.svelte";
+	import Button from "./Button.svelte";
+	import Inputs from "./Inputs.svelte";
 	export let name;
 	let lenguaje = 'Pronto estaremos con ustedes';
 	let examplearray = {
@@ -7,16 +10,53 @@
 		apellido:`Fernandez`
 	}
 
+	let disabled = true;
+	let propertyInput = {
+		placeholder : `Inputs`,
+		type : `text`,
+		className : `form`,
+		require : `required`,
+		value:''
+	};
+	
+
+	// Funciones
+	const HandleClick = () => {
+		name = 'Desarrollador';
+		propertyInput.value = '1089547521'
+	}
+
 </script>
+
+
 
 <main>
 	<h1>Modo desarrollo {name}!</h1>
 	<Datos  lenguaje={lenguaje} nombre={examplearray.nombre} apellido={examplearray.apellido} />
 	<!-- 	<Datos  {...examplearray} />-->
+
+	<!-- Botones -->
+	<Button {disabled} {HandleClick}/>
+
+	<!-- Inputs -->
+	<Inputs {...propertyInput}/>
+	<!--{console.log(propertyInput)}-->
+	<!--{@debug propertyInput}-->
+
 	
+
 </main>
 
 <style>
+
+	:global(input){
+		border-radius: 20px;
+		background-color: burlywood;
+		padding: 3px 10px 3px 10px;
+		box-sizing: border-box;
+		outline: none;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -30,6 +70,7 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
 
 	@media (min-width: 640px) {
 		main {
---
+++ src/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let disabled = false;
+    export let HandleClick = 'p';
+</script>
+
+<style>
+
+</style>
+
+<main>
+    <h1>Pasando propiedades a para un elemento html</h1>
+    <button type="button" disabled={disabled}>Disabled</button>
+    <button type="button" on:click={HandleClick}>Normal</button>
+</main>
