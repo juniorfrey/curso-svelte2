8c361bcea5426defebbfcd58db3132b66a05c4c4
Juanito perez <Juanito@hotmail.com>
2021-08-12 15:14:14 -0500

    Clase 10 'Stores parte I' se practico como crear un store con svelte, se interactuo con la función writetable

--- src/Ruta_contacto.svelte
+++ src/Ruta_contacto.svelte
@@ -1,8 +1,20 @@
 <script>
+	import { numero } from './stores/store.js';
+    
     export let id;
+    let valor_numero;
+    numero.subscribe((value) => {
+        valor_numero = value
+    });
+
+    function reset(){
+        numero.set(0);
+    }
 </script>
 
 <h2>Contacto</h2>
+<p>{valor_numero}</p>
+<button on:click={reset}>Reset</button>
 
 {#if id}
     <p>Usuario: {id}</p>
--- src/Ruta_home.svelte
+++ src/Ruta_home.svelte
@@ -1,1 +1,22 @@
+<script>
+    import { onDestroy } from 'svelte';
+
+	import { numero } from './stores/store.js';
+ 
+
+    /*let valor_numero;
+    const unsubscribe = numero.subscribe((value) => {
+        valor_numero = value
+    });*/
+
+    function incrementar(){
+        numero.update((n) => n + 1)
+    }
+
+   // onDestroy(unsubscribe);
+
+</script>
 <h2>Home</h2>
+<p>{$numero}</p>
+
+<button on:click={incrementar}>Sumar</button>
